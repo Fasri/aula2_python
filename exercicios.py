43882857fc4d435a34b47b6ce3aff93de3c8e25e
Fasri <felipeasr@gmail.com>
2025-08-22 08:47:07 -0300

    exercicio 7

--- exercicios.py
+++ exercicios.py
@@ -32,13 +32,18 @@
 # #### Números de Ponto Flutuante (`float`)
 
 # 6. Escreva um programa que receba dois números flutuantes e realize sua adição.
-numero1 = float(input("Digite o primeiro número flutuante: "))
-numero2 = float(input("Digite o segundo número flutuante: "))
-soma = numero1 + numero2
+# numero1 = float(input("Digite o primeiro número flutuante: "))
+# numero2 = float(input("Digite o segundo número flutuante: "))
+# soma = numero1 + numero2
 
-print("A soma dos números flutuantes é:", soma)
+# print("A soma dos números flutuantes é:", soma)
 
 # 7. Crie um programa que calcule a média de dois números flutuantes fornecidos pelo usuário.
+numero1 = float(input("Digite o primeiro número flutuante: "))
+numero2 = float(input("Digite o segundo número flutuante: "))
+media = (numero1 + numero2) /2
+
+print(f"A media  dos números flutuantes, {numero1}  e {numero2},  é:", media)
 # 8. Desenvolva um programa que calcule a potência de um número (base e expoente fornecidos pelo usuário).
 # 9. Faça um programa que converta a temperatura de Celsius para Fahrenheit.
 # 10. Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
