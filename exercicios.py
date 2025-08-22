fe9c4d93f183ca0ff3f1b35baf970c4a9d05b91a
Fasri <felipeasr@gmail.com>
2025-08-22 08:34:18 -0300

    execicio 3

--- exercicios.py
+++ exercicios.py
@@ -8,11 +8,16 @@
 # print("A soma dos números é:", soma)
 
 # 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
-numero = int(input("Digite um número: "))
-resto = numero % 5
-print("O resto da divisão por 5 é:", resto)
+# numero = int(input("Digite um número: "))
+# resto = numero % 5
+# print("O resto da divisão por 5 é:", resto)
 
 # 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+numero1 = int(input("Digite o primeiro número:"))
+numero2 = int(input( "Digite o segundo número"))
+produto = numero1 * numero2
+print("A mutiplicação dos números é:", produto)
+
 # 4. Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 # 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 
